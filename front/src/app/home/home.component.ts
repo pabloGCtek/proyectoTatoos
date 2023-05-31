b8d3089c756507eccb76bfec4bad8f94dce594a0
pablo.garciacervigon <pablo.garciacervigon@softtek.com>
2023-05-31 13:35:06 +0200

    artistas cargan en pag principal

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Artista } from 'src/assets/clases/Artista';
+import { ArtistasService } from 'src/assets/servicios/artistas.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,12 @@ import { Artista } from 'src/assets/clases/Artista';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  arrArtistas: Artista[] = [new Artista("pepe","Intro para el artista va aquí","../assets/media/tatuador1.jpg"),new Artista("Juana","Lorem ipsum y tal","../assets/media/tatuador2.jpg")];
-  NgOnInit(){
-    alert(this.arrArtistas[0].nombre)
+  arrArtistas: Artista[] = [];
+  constructor(private servicioArt: ArtistasService){
+
   }
+  ngOnInit(){
+  this.servicioArt.mostrarArtista().subscribe((val: any) => this.arrArtistas = val)
+  }
+  //rrArtistas: Artista[] = [new Artista("pepe","Intro para el artista va aquí","../assets/media/tatuador1.jpg"),new Artista("Juana","Lorem ipsum y tal","../assets/media/tatuador2.jpg")];
 }
--- front/src/assets/clases/Artista.ts
+++ front/src/assets/clases/Artista.ts
@@ -1,13 +1,31 @@
+import { Tattoo } from "./Tattoo"
 export class Artista{
-    idArtista: number;
-    intro: String;
-    imagen: String;
-    nombre: String;
 
-    constructor(nombre: String, intro:String, imagen:String){
-        
-        this.intro = intro;
-        this.nombre = nombre;
-        this.imagen = imagen;
-    }
+  idArtista:number = 0
+
+  nombre:string=""
+
+  bio:string=""
+
+  imagen:string=""
+
+  tattoos:Tattoo[] = []
+
+  constructor(idArtista: number, nombre:string,bio:string,imagen:string,tattoos: Tattoo[]){
+
+    this.idArtista=idArtista
+
+    this.nombre=nombre
+
+    this.bio=bio
+
+    this.imagen=imagen
+
+    this.tattoos=tattoos
+
+
+
+
+  }
+
 }
