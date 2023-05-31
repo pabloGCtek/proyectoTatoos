02a1572bf4f5deecaec36c26bbf9e178939d2288
raulgomezcano <raul.gomezc@softtek.com>
2023-05-31 13:44:44 +0200

    Añadido servicio artista

---
+++ front/src/app/artistas.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Artista } from './clases/Artista';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistasService {
+  constructor(private http: HttpClient) { }
+  private artistaUrl: string ="http://localhost:8080/artistas"
+
+  // artistas: Artista[]=[]
+  mostrarArtista():Observable<Artista[]>
+{
+    return this.http.get<Artista[]>(this.artistaUrl)
+}
+}
