9229c718a1e04a1197b7acf8e5ae9ad9dffa8806
ALCstkgit <adrian.lopezc@softtek.com>
2023-06-09 14:29:25 +0200

    PushBack4-Ultima Cita

--- back/src/main/java/com/softtek/tattoos_proyecto/repository/ICitaRepository.java
+++ back/src/main/java/com/softtek/tattoos_proyecto/repository/ICitaRepository.java
@@ -1,6 +1,9 @@
 package com.softtek.tattoos_proyecto.repository;
 
 import com.softtek.tattoos_proyecto.model.Cita;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ICitaRepository extends IGenericRepo<Cita, Integer> {
+    @Query(value = "FROM Cita c ORDER BY c.idCita DESC LIMIT 1")
+    Cita findLastCita();
 }
